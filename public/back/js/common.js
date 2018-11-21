8aeaabe6668f3f469f399af763cd0c1eed8e97b1
rediceice <178327786@qq.com>
2018-11-21 19:34:22 +0800

    新文件

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,4 @@
+
 $(function(){
 
   // NProgress.start();
@@ -21,4 +22,38 @@ $(function(){
     NProgress.done();
   })
 
+})
+
+
+//左侧二级切换
+$('.cate').click(function(){
+  $(this).next().slideToggle();
+})
+
+$('.lt_topbar .icon_left').click(function(){
+  $('.lt_aside').toggleClass('hidemenu');
+  $('.lt_main').toggleClass('hidemenu');
+  $('.lt_topbar').toggleClass('hidemenu');
+})
+
+$('.lt_topbar .icon_right').click(function(){
+  $('#myModal').modal('show');
+  
+})
+
+$('.btn-out').click(function(){
+  $.ajax({
+    type:'get',
+    url:'/employee/employeeLogout',
+    dataType:'json',
+    success:function(info){
+      // console.log(info);
+      if(info.success){
+        location.href ='login.html';
+      }
+      
+      
+    }
+  })
+  
 })
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,15 @@
+
+$.ajax({
+  type:'get',
+  url:'/employee/checkRootLogin',
+  dataType:'json',
+  success:function(info){
+    if(info.success){
+      console.log('用户已登录');
+      
+    }
+    if(info.error===400){
+      location.href = 'login.html';
+    }
+  }
+})
